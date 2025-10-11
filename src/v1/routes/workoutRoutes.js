85ad0ea17f7fb2b995ef23869dccc6bef0e91cf3
alfonso.castano <alfonsocastano@hotmail.com>
2025-10-11 16:36:19 -0500

    Import apicache and create a cache for the app

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,10 +1,9 @@
 import express from "express";
-import apicahce from "apicahce";
+import apicache from "apicache";
 import { createNewWorkout, deleteOneWorkout, getAllWorkouts, getOneWorkout, updateOneWorkout } from "../../controllers/workoutController.js";
 import { getRecordsForWorkout } from "../../controllers/recordController.js";
 const router = express.Router();
-const cache = apicahce.middleware;
-
+const cache = apicache.middleware;
 router
     .get("/", cache("2 minutes"), getAllWorkouts)
     .get("/", getAllWorkouts)
