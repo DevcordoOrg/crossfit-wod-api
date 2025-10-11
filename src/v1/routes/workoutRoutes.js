f0cf02905f6ab8b856f30017913257a4bdc298ff
alfonso.castano <alfonsocastano@hotmail.com>
2025-10-11 16:24:10 -0500

    Installation of apicache Import apicache into workoutRoutes Create a cache and add to to routes

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
+import apicahce from "apicahce";
 import { createNewWorkout, deleteOneWorkout, getAllWorkouts, getOneWorkout, updateOneWorkout } from "../../controllers/workoutController.js";
 import { getRecordsForWorkout } from "../../controllers/recordController.js";
 const router = express.Router();
+const cache = apicahce.middleware;
+
 router
+    .get("/", cache("2 minutes"), getAllWorkouts)
     .get("/", getAllWorkouts)
     .get("/:workoutId", getOneWorkout)
     .get("/:workoutId/records", getRecordsForWorkout)
